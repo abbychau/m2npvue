117a8c65eca3e89ba605ca5feb3ffed180b9e8fe
Abby <abbychau@gmail.com>
2019-11-18 01:30:32 +0900

    add cookie

--- src/main.js
+++ src/main.js
@@ -6,30 +6,32 @@ import router from './router'
 import store from './store'
 import FBSignInButton from 'vue-facebook-signin-button'
 import 'ant-design-vue/dist/antd.css'
-
+import VueCookie from 'vue-cookie'
 import './directives'
 
+
+Vue.use(VueCookie)
 Vue.config.productionTip = false
 Vue.use(Antd)
 
 Vue.use(FBSignInButton)
 
-
 var Auth = {
   loggedIn: false,
-  login: function() { this.loggedIn = true },
-  logout: function() { this.loggedIn = false }  
-};
+  login: function () { this.loggedIn = true },
+  logout: function () { this.loggedIn = false }
+}
 
 router.beforeEach((to, from, next) => {
+  next()
   if (to.matched.some(record => record.meta.requiresAuth) && !Auth.loggedIn) {
-    next({ path: '/login', query: { redirect: to.fullPath }});
-  }else if (to.matched.some(record => record.meta.loggedInThenRedirect) && Auth.loggedIn) {
-    next({ path: record.meta.loggedInThenRedirect });
+    next({ path: '/login', query: { redirect: to.fullPath } })
+  } else if (to.matched.some(record => record.meta.loggedInThenRedirect) && Auth.loggedIn) {
+    next({ path: record.meta.loggedInThenRedirect })
   } else {
-    next();
+    next()
   }
-});
+})
 
 new Vue({
   router,
