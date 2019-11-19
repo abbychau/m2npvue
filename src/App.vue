e78dfa6f1943bd240a961666fd1f67f6641db506
Abby <abbychau@gmail.com>
2019-11-19 23:01:14 +0900

    remove antd

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,50 @@
 <template>
-  <a-layout id="components-layout-demo-fixed-sider">
-    <a-layout-sider :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }">
+  <div id="components-layout-demo-fixed-sider">
+    <div :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }">
       <div class="logo"></div>
-      <a-menu theme="light" mode="inline" :defaultSelectedKeys="['4']">
-        <a-menu-item key="1">
-          <router-link to="/">
-            <a-icon type="user" />
-            <span class="nav-text">Home</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <router-link to="/about">
-            <a-icon type="video-camera" />
-            <span class="nav-text">About</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <router-link to="/login">
-            <a-icon type="upload" />
-            <span class="nav-text">Login</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="4">
-          <router-link to="/afterlogin">
-            <a-icon type="bar-chart" />
-            <span class="nav-text">After Login</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="5">
-          <router-link to="/login/facebook/callback">
-            <a-icon type="bar-chart" />
-            <span class="nav-text">Callback</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="6">
-          <a-icon type="appstore-o" />
-          <span class="nav-text">nav 6</span>
-        </a-menu-item>
-        <a-menu-item key="7">
-          <a-icon type="team" />
-          <span class="nav-text">nav 7</span>
-        </a-menu-item>
-        <a-menu-item key="8">
-          <router-link to="/settings">
-            <a-icon type="bar-gear" />
-            <span class="nav-text">Settings</span>
-          </router-link>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
-    <a-layout :style="{ marginLeft: '200px' }">
-      <a-layout-header :style="{ background: '#fff', height:'2em', lineHeight:'2em' }">testing</a-layout-header>
-      <a-layout-content :style="{ margin: '24px 16px 0', overflow: 'initial' }">
+      <nav class='sidemenu'>
+        <ul>
+          <li>
+        <router-link to="/">
+          <a-icon type="user" />
+          <span class="nav-text">Home</span>
+        </router-link>
+        </li><li>
+        <router-link to="/about">
+          <a-icon type="video-camera" />
+          <span class="nav-text">About</span>
+        </router-link>
+        </li><li>
+        <router-link to="/login">
+          <a-icon type="upload" />
+          <span class="nav-text">Login</span>
+        </router-link>
+        </li><li>
+        <router-link to="/afterlogin">
+          <a-icon type="bar-chart" />
+          <span class="nav-text">After Login</span>
+        </router-link>
+        </li><li>
+        <router-link to="/login/facebook/callback">
+          <a-icon type="bar-chart" />
+          <span class="nav-text">Callback</span>
+        </router-link>
+        </li><li>
+        <router-link to="/settings">
+          <a-icon type="bar-gear" />
+          <span class="nav-text">Settings</span>
+        </router-link>
+        </li></ul>
+      </nav>
+    </div>
+    <div :style="{ marginLeft: '200px' }">
+      <div :style="{ background: '#fff', height:'2em', lineHeight:'2em' }">testing</div>
+      <div :style="{ margin: '24px 16px 0', overflow: 'initial' }">
         <router-view />
-      </a-layout-content>
-      <a-layout-footer :style="{ textAlign: 'center' }">m2np.com</a-layout-footer>
-    </a-layout>
-  </a-layout>
+      </div>
+      <div :style="{ textAlign: 'center' }">m2np.com</div>
+    </div>
+  </div>
 </template>
 
 <style>
@@ -66,16 +54,7 @@
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.sidemenu router-link{
+  display:block
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
-import Antd from 'ant-design-vue'
-
 import App from './App'
 import router from './router'
 import store from './store'
 import FBSignInButton from 'vue-facebook-signin-button'
-import 'ant-design-vue/dist/antd.css'
 import VueCookies from 'vue-cookies'
 import './directives'
 
 
 Vue.use(VueCookies)
 Vue.config.productionTip = false
-Vue.use(Antd)
 Vue.use(FBSignInButton)
 VueCookies.config('7d')
 
